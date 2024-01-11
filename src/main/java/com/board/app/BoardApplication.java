e7f1a9b2723b852504ad5c07157eca9820dfed57
sensen52 <gytls52tpdls@naver.com>
2024-01-11 14:54:27 +0900

    mapperBean error issue solve

--- src/main/java/com/board/app/BoardApplication.java
+++ src/main/java/com/board/app/BoardApplication.java
@@ -1,9 +1,11 @@
 package com.board.app;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@MapperScan("com.board.app.mapper")
 public class BoardApplication {
 
     public static void main(String[] args) {
